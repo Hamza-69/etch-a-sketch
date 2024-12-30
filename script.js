2997d807fef588b8d7640dd20b06dd5ec6be6923
Hamza El Rachdi <hamzarachidi04@gmail.com>
2024-12-30 16:44:50 +0200

    Add Image Buttons and Suitable Styling

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ let makeGrid = (n) => {
     containercolor.innerHTML += "<div class=\"left-border\"></div>\n<div class=\"right-border\"></div>"
     let boxes = document.querySelectorAll(".container-color div")
     boxes.forEach( item => {
+      let isMouseDown;
         item.addEventListener("mousedown", () => {
             isMouseDown = true;
             color(item);
@@ -29,3 +30,46 @@ let makeGrid = (n) => {
     })
     };
 makeGrid(16);
+let containertools = document.querySelector(".container-tools");
+const images = {
+    "save-image.png": () => {
+      console.log("Save image action triggered");
+      // Add logic to save the image
+    },
+    "back.png": () => {
+      console.log("Back action triggered");
+      // Add logic for the "back" action
+    },
+    "mirror.png": () => {
+        console.log("Mirror action triggered");
+        // Add logic for "Mirror"
+      },
+    "choose-trans.png": () => {
+      console.log("Choose transparent action triggered");
+      // Add logic for "Choose Transparent"
+    },
+    "vari-trans.png": () => {
+      console.log("Variable transparent action triggered");
+      // Add logic for variable transparency
+    },
+    "fill.png": () => {
+      console.log("Fill action triggered");
+      // Add logic for "Fill"
+    },
+    "erase.png": () => {
+        console.log("Erase action triggered");
+        // Add logic for "Erase"
+      },
+    "print-image.png": () => {
+      console.log("Print image action triggered");
+      // Add logic to print the image
+    }
+}
+for (let imageSrc in images) {
+    let image = document.createElement("img");
+    image.src = "images/"+imageSrc;
+    image.height *= 0.28;
+    image.className = "click image";
+    image.addEventListener("click", images[imageSrc]);
+    containertools.appendChild(image);
+}
