5089e7b69fcbdfe6b7c22891d986bc29b401c145
Hamza El Rachdi <hamzarachidi04@gmail.com>
2024-12-30 15:26:42 +0200

    Complete CSS and HTML Basic Layout

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let container = document.querySelector(".container");
+let container = document.querySelector(".container-color");
 let makeGrid = (n) => {
     let opacity = 0;
     container.innerHTML = "";
@@ -9,11 +9,12 @@ let makeGrid = (n) => {
     for (let i =0; i<n; i++) 
         for (let j=0;j<n;j++) {
             let box = document.createElement("div");
-            box.style.width = 960/n + "px";
-            box.style.height = 960/n + "px";
+            box.style.width = 720/n + "px";
+            box.style.height = 720/n + "px";
             container.appendChild(box);
         }
-    let boxes = document.querySelectorAll(".container div")
+    container.innerHTML += "<div class=\"left-border\"></div>\n<div class=\"right-border\"></div>"
+    let boxes = document.querySelectorAll(".container-color div")
     boxes.forEach( item => {
         item.addEventListener("mouseenter", () => {
             let red = Math.floor(Math.random()*256), green = Math.floor(Math.random()*256), blue = Math.floor(Math.random()*256);
