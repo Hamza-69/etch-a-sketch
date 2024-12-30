169633af387d89110a96a3f2f93b1b7586f7aa09
Hamza El Rachdi <hamzarachidi04@gmail.com>
2024-12-30 15:50:29 +0200

    Implement dynamic coloring logic
    Coloring depends on choosen tool.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let container = document.querySelector(".container-color");
+let containercolor = document.querySelector(".container-color");
 let makeGrid = (n) => {
     let opacity = 0;
-    container.innerHTML = "";
+    containercolor.innerHTML = "";
     if (n > 100 || n <= 0) {
         alert("Invalid Value!! Make sure that n is at most 100!");
         n = prompt("Enter new number of squares ( 0<n<=100)");
@@ -11,19 +11,21 @@ let makeGrid = (n) => {
             let box = document.createElement("div");
             box.style.width = 720/n + "px";
             box.style.height = 720/n + "px";
-            container.appendChild(box);
+            containercolor.appendChild(box);
         }
-    container.innerHTML += "<div class=\"left-border\"></div>\n<div class=\"right-border\"></div>"
+    containercolor.innerHTML += "<div class=\"left-border\"></div>\n<div class=\"right-border\"></div>"
     let boxes = document.querySelectorAll(".container-color div")
     boxes.forEach( item => {
-        item.addEventListener("mouseenter", () => {
-            let red = Math.floor(Math.random()*256), green = Math.floor(Math.random()*256), blue = Math.floor(Math.random()*256);
-            item.style.backgroundColor = `rgba(${red}, ${blue}, ${green}, ${(opacity < 100? opacity+=10: opacity=10)/100})`
+        item.addEventListener("mousedown", () => {
+            isMouseDown = true;
+            color(item);
+        });
+        item.addEventListener("mouseover", () => {
+            if (isMouseDown) color(item);
+        });
+        item.addEventListener("mouseup", () => {
+            isMouseDown = false; 
         });
     })
     };
 makeGrid(16);
-let button = document.querySelector("button");
-button.addEventListener("click", () => {
-    makeGrid(prompt("Enter new number of squares ( 0<n<=100)"))
-})
